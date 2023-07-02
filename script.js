8ef507f8677669530b1b88b307b7dce0bb3e5c4c
Arnold W <arnoldwwong@gmail.com>
2023-07-03 00:36:44 +1000

    Edit comp choice to anon

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const rpsChoice = ["rock", "paper", "scissors"];
+//const rpsChoice = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3);
-    return rpsChoice[computerChoice];
+    //const computerChoice = Math.floor(Math.random() * rpsChoice.length);
+    //return rpsChoice[computerChoice];
+    const rpsChoice = ['rock', 'paper', 'scissors'];
+    return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        alert("Tie!");
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        alert("Winner!");
+    } else {
+        alert("LOSER!");
+    }  
+}
+    const playerChoice = 'rock';
+    const computerChoice = getComputerChoice();
+
+console.log(computerChoice);    
+console.log(playerChoice);    
+console.log(playRound(playerChoice, computerChoice));
