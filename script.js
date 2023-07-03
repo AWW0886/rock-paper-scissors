c38484b4f346597f25dd0771d44cc37059f23dae
Arnold W <arnoldwwong@gmail.com>
2023-07-03 14:35:04 +1000

    Add Friends reference

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
 //const rpsChoice = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     //const computerChoice = Math.floor(Math.random() * rpsChoice.length);
     //return rpsChoice[computerChoice];
-    const rpsChoice = ['rock', 'paper', 'scissors'];
-    return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];
+    //if (playerChoice === 'rock' || 'paper' || 'scissors') {
+    //    let rpsChoice = ['rock', 'paper', 'scissors'];
+    //    return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];
+    //} else if (playerChoice === "fire") {
+    //    let rpsChoice = ['rock', 'paper', 'scissors', 'water balloon'];
+    //    return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];    
+    //}
+    if (playerChoice === "fire") {
+        let rpsChoice = ['rock', 'paper', 'scissors', 'water balloon'];
+        return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];    
+    } else if (playerChoice === 'rock' || 'paper' || 'scissors') {
+        let rpsChoice = ['rock', 'paper', 'scissors'];
+        return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];
+    }
 }
 
 //console.log(getComputerChoice());
 
+function game() {}
+    //for (let i = 0; i < 5; i++) {
+    
+
+
 function playRound(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        alert("Tie!");
-    } else if (
-        (playerChoice === 'rock' && computerChoice === 'scissors') ||
-        (playerChoice === 'paper' && computerChoice === 'rock') ||
-        (playerChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-        alert("Winner!");
-    } else {
-        alert("LOSER!");
-    }  
+    //for (let i = 0; i < 5; i++) {
+    
+        if (playerChoice === computerChoice) {
+            alert('Tie!');
+        } else if (
+            (playerChoice === 'rock' && computerChoice === 'scissors') ||
+            (playerChoice === 'paper' && computerChoice === 'rock') ||
+            (playerChoice === 'scissors' && computerChoice === 'paper') ||
+            (playerChoice === 'fire' && computerChoice !== 'water balloon')
+        ) {
+            alert('Winner!');
+            playerScore++;
+        } else {
+            alert('LOSER!');
+            computerScore++;
+        }
+    //}  
 }
-    const playerChoice = 'rock';
+
+    const playerChoice = prompt();
     const computerChoice = getComputerChoice();
 
-console.log(computerChoice);    
-console.log(playerChoice);    
+console.log(playerChoice);  
+console.log(computerChoice);   
 console.log(playRound(playerChoice, computerChoice));
+console.log(playerScore);
+console.log(computerScore);
