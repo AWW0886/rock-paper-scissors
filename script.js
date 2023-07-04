64f6cf2703221527603454c9040c77d475ce67c7
Arnold W <arnoldwwong@gmail.com>
2023-07-04 20:33:51 +1000

    Solve x5 game call

--- script.js
+++ script.js
@@ -2,35 +2,21 @@
 let playerScore = 0;
 let computerScore = 0;
 
-function getComputerChoice() {
-    //const computerChoice = Math.floor(Math.random() * rpsChoice.length);
-    //return rpsChoice[computerChoice];
-    //if (playerChoice === 'rock' || 'paper' || 'scissors') {
-    //    let rpsChoice = ['rock', 'paper', 'scissors'];
-    //    return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];
-    //} else if (playerChoice === "fire") {
-    //    let rpsChoice = ['rock', 'paper', 'scissors', 'water balloon'];
-    //    return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];    
-    //}
-    if (playerChoice === "fire") {
-        let rpsChoice = ['rock', 'paper', 'scissors', 'water balloon'];
-        return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];    
-    } else if (playerChoice === 'rock' || 'paper' || 'scissors') {
-        let rpsChoice = ['rock', 'paper', 'scissors'];
-        return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+    function getComputerChoice() {
+        if (playerChoice === "fire") {
+            let rpsChoice = ['rock', 'paper', 'scissors', 'water balloon'];
+            return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];    
+        } else if (playerChoice === 'rock' || 'paper' || 'scissors') {
+            let rpsChoice = ['rock', 'paper', 'scissors'];
+            return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];
+        }
     }
-}
+    //console.log(getComputerChoice());
 
-//console.log(getComputerChoice());
-
-function game() {}
-    //for (let i = 0; i < 5; i++) {
-    
-
-
-function playRound(playerChoice, computerChoice) {
-    //for (let i = 0; i < 5; i++) {
-    
+    function playRound(playerChoice, computerChoice) {
         if (playerChoice === computerChoice) {
             alert('Tie!');
         } else if (
@@ -41,18 +27,27 @@ function playRound(playerChoice, computerChoice) {
         ) {
             alert('Winner!');
             playerScore++;
+        } else if (
+            (playerChoice === 'fire' && computerChoice === 'water balloon')
+        ) {
+            alert('LOSER!');
+            alert("Aww... Well played, Phoebe Buffay!");
+            computerScore++;
         } else {
             alert('LOSER!');
             computerScore++;
-        }
-    //}  
-}
+        } 
+    }
 
-    const playerChoice = prompt();
+    const playerChoice = prompt().toLowerCase();
     const computerChoice = getComputerChoice();
 
-console.log(playerChoice);  
-console.log(computerChoice);   
-console.log(playRound(playerChoice, computerChoice));
-console.log(playerScore);
-console.log(computerScore);
+    console.log(playerChoice);  
+    console.log(computerChoice);   
+    console.log(playRound(playerChoice, computerChoice));
+    console.log(playerScore);
+    console.log(computerScore);
+    }
+}
+
+console.log(game());
