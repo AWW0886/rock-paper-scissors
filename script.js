932ef38865964416887c537064905166d5afffd3
Arnold W <arnoldwwong@gmail.com>
2023-07-08 17:19:32 +1000

    Edit mildly... looks not good...

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function game() {
     //for (let i = 0; i < 5; i++) {
 
     function getComputerChoice() {
-        if (playerChoice === "fire") {
+        if (playerChoice === 'fire') {
             let rpsChoice = ['rock', 'paper', 'scissors', 'water balloon'];
             return rpsChoice[Math.floor(Math.random() * rpsChoice.length)];    
         } else if (playerChoice === 'rock' || 'paper' || 'scissors') {
@@ -20,6 +20,7 @@ function game() {
     function playRound(playerChoice, computerChoice) {
         if (playerChoice === computerChoice) {
             alert('Tie!');
+            alert(playerChoice + ' = ' + computerChoice);
         } else if (
             (playerChoice === 'rock' && computerChoice === 'scissors') ||
             (playerChoice === 'paper' && computerChoice === 'rock') ||
@@ -27,6 +28,7 @@ function game() {
             (playerChoice === 'fire' && computerChoice !== 'water balloon')
         ) {
             alert('Winner!');
+            alert(playerChoice + ' > ' + computerChoice);
             playerScore++;
         } else if (
             (playerChoice === 'fire' && computerChoice === 'water balloon')
@@ -36,34 +38,38 @@ function game() {
             computerScore++;
         } else {
             alert('LOSER!');
+            alert(playerChoice + ' < ' + computerChoice);
             computerScore++;
         } 
     }
 
     //const playerChoice = prompt().toLowerCase();
-    //const computerChoice = getComputerChoice();
+    const computerChoice = getComputerChoice();
 
     //console.log(playerChoice);  
-    //console.log(computerChoice);   
-    //console.log(playRound(playerChoice, computerChoice));
-    //console.log(playerScore);
-    //console.log(computerScore);
+    console.log(computerChoice);   
+    console.log(playRound(playerChoice, computerChoice));
+    console.log(playerScore);
+    console.log(computerScore);
     //}
 }
 
 //console.log(game());
 
 const buttons = document.querySelectorAll('button');
-const btn = document.querySelector('#btn');
+const btn = document.querySelector('button');
 
-buttons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        alert("Works!");
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerChoice = button.id;
+        console.log(playerChoice);
+        game(playerChoice);
     });
 });
+
 
 // Make buttons
 // Add event listeners to buttons
 // Increase score
 // Display score (add div)
-// End game when score === 5
+// End game function when score === 5
